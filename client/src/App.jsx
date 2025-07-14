931bf5c6740afbeeecfd550270a6cf34be208ec3
justdk06 <justdk06@gmail.com>
2025-07-14 19:41:23 +0530

    feat: integrate Clerk for authentication and add user management to Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import Navbar from './components/Navbar';
-import { Route,Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = () =>{
