499d938d92722f590891a1e23bedfbfe73370b1f
justdk06 <justdk06@gmail.com>
2025-07-16 18:35:30 +0530

    Updates AddRoom

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Title from "../../components/Title";
 
 const AddRoom = ()=>{
-  return (
-    <div>
 
-    </div>
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null
+  })
+  const [inputs, setInputs] = useState({
+    roomType: '',
+    pricePerNight: 0,
+    amenities:{
+      'Free WiFi': false,
+      'Free Breakfast': false,
+      'Room Service': false,
+      'Mountain View': false,
+      'Pool Access': false,
+    }
+  })
+  return (
+    <form>
+      <Title align='left' font='outfit' title='Add Room' subTitle='Fill in the details carefully and accurate room details, pricing, and amenities, to enhance the user booking experience.'/>
+    </form>
   )
 }
 
