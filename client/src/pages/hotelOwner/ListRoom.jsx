3a79265ad2b4f4843e906c825bac774112e3c719
justdk06 <justdk06@gmail.com>
2025-07-16 19:30:41 +0530

    changes updates

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -5,7 +5,7 @@ import Title from "../../components/Title";
 const ListRoom = ()=>{
   const [rooms, setRooms] = useState(roomsDummyData)
   return (
-    <div>
+    <div className="pt-6">
       <Title align='left' font='outfit' title='Room Listings' subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users.' />
       <p className="text-gray-500 mt-8">All Rooms</p>
 
