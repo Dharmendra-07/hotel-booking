5dbd64e885834a837ce0051e773c707c47f3992a
justdk06 <justdk06@gmail.com>
2025-07-16 16:38:10 +0530

    feat: add Navbar and Layout components for hotel owner interface

---
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+
+const Layout = ()=>{
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar />
+    </div>
+  )
+}
+
+
+export default Layout;
